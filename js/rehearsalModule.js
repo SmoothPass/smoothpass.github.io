37f504ba3ea7953fbdcc61da94fcc1f6a9e24c31
aerinzhang <shikunz@andrew.cmu.edu>
2015-01-07 10:55:51 -0800

    fixing

--- js/rehearsalModule.js
+++ js/rehearsalModule.js
@@ -185,8 +185,8 @@ var rehearsalModule = ( function () {
 					placeholder='doing what'/></span><br><br><div \
 					class=halfbuttonDiv><a data-role='button' \
 					id='gameCheckNextButton' tabindex='3' class=right \
-					onclick='rehearsalModule.rehearseStory(" + person + ", " 
-							+ scene + ")' >Rehearse</a>\
+					onclick='rehearsalModule.rehearseStory('" + person + "', '" 
+							+ scene + "')' >Rehearse</a>\
 					<a href='#' class=left data-role='button' tabindex='4' \
 					onclick='recoveryMechanism.recoverStory()'>I Forget</a>\
 					</div></span></form></span></div></div>");
