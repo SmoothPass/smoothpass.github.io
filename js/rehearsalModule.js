1537106996c08e3d1ecabff69ddbea019417247c
aerinzhang <shikunz@andrew.cmu.edu>
2015-01-12 10:41:44 -0800

    updating board

--- js/rehearsalModule.js
+++ js/rehearsalModule.js
@@ -77,6 +77,9 @@ var rehearsalModule = ( function () {
 		}
 		$("#rehearsal-password").val('');
 		$("#rehearsal-password-b").val('');
+		//update board
+		checkEachStory();
+		renderRehearsalBoard();
 		$.mobile.changePage('#board');
 	}
 
