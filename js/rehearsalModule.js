b67d2c5a8086e34b75a20b2fef8cfbc4db6e5538
aerinzhang <shikunz@andrew.cmu.edu>
2015-03-02 11:24:36 +0800

    fixing page renewal and add enter

--- js/rehearsalModule.js
+++ js/rehearsalModule.js
@@ -389,7 +389,7 @@ var rehearsalModule = ( function () {
 		$('#rehearsalButtonDiv').html(bDiv);
 		//update box css 
 		$('.boxWidget div').removeClass();
-		$(pageID).on('keypress', 'rehearseButton', function(args) {
+		$(pageID).on('keypress', '#rehearseButton', function(args) {
 			if (args.keyCode == 13) {
 				$("#rehearseButton").click();
 				return false;
